b834faffe14e4a495f41bd17e80974077c969b50
masonmarker <markermd@dukes.jmu.edu>
2022-06-24 20:46:20 -0400

    First Official Deployment

--- application/src/App.js
+++ application/src/App.js
@@ -1,15 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  document.body.style = 'background: white;';
   return (
     <div className="App">
-      <p>welcome to my site!</p>
-      <p>test push at 3:40</p>
-      <p>test push at 3:49</p>
-      <p>test push at 3:51</p>
+      <style>
+         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;700;800&display=swap');
+      </style>
+      <NavBar /> 
+      <h1 className="name-title">— Mason Marker —</h1>
+      <About />
+      <Footer />
     </div>
   );
 }
 
-export default App;
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <h className="navbar-title">Hi! Most people call me..</h>
+    </div>
+  );
+}
+
+const About = () => {
+  return (
+    <div className="about-div">
+      <h1>Who Am I?</h1>
+      <p>I'm a software engineer with a passion for creating beautiful, responsive websites.</p>
+      <p className="increased-margin-bottom">I'm currently working as an RPA engineer at <a href="https://www.genworth.com/" target="_blank" >Genworth</a>.</p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Mason Marker</p>
+      <p>2022</p>
+    </div>
+  )
+}
+
+
+export default App; 
